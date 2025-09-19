c205aa286819e62fbe63699b6fd4d8e7bd419186
SadekBlk <sadek.belkhiria@polytechnicien.tn>
2025-09-19 10:53:56 +0200

    Improving application performance through Redis caching and managing network traffic with a rate-limiting strategy.

--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -10,6 +10,9 @@ import com.pm.patientservice.kafka.KafkaProducer;
 import com.pm.patientservice.mapper.PatientMapper;
 import com.pm.patientservice.model.Patient;
 import com.pm.patientservice.repository.PatientRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -24,6 +27,7 @@ import java.util.UUID;
 @Service
 public class PatientService {
 
+    private static final Logger log = LoggerFactory.getLogger(PatientService.class);
     private final PatientRepository patientRepository;
     private final BillingServiceGrpcClient billingServiceGrpcClient;
     //private final com.pm.patientservice.kafka.kafkaProducer kafkaProducer;
@@ -38,7 +42,21 @@ public class PatientService {
         this.kafkaProducer = kafkaProducer;
     }
 
+    @Cacheable(
+            value = "patients",
+            key = "#page + '-' + #size + '-' + #sort + '-' + #sortField",
+            condition = "#searchValue == ''"
+    )
     public PagedPatientResponseDTO getPatients(int page, int size, String sort, String sortField, String searchValue) {
+
+//        log.info("[_REDIS_]: Cache miss - fetching from DB instead");
+//
+//        try {
+//            Thread.sleep(3600);
+//        } catch (InterruptedException e) {
+//            log.error(e.getMessage());
+//        }
+
         Pageable pageable = PageRequest.of(page - 1, size,
                 sort.equalsIgnoreCase("desc")
                         ? Sort.by(sortField).descending()
